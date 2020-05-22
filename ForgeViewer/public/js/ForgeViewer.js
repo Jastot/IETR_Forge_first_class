338176b4475c2bf973a838cdd88ac3c983b01367
Matthew <matvey.kalinin2001@mail.ru>
2020-05-22 15:18:07 +0300

    изменили размер appBucket

--- ForgeViewer/public/js/ForgeViewer.js
+++ ForgeViewer/public/js/ForgeViewer.js
@@ -9,7 +9,7 @@ function launchViewer(urn) {
   Autodesk.Viewing.Initializer(options, () => {
     viewer = new Autodesk.Viewing.GuiViewer3D(document.getElementById('forgeViewer'), { extensions: [ 'Autodesk.DocumentBrowser'] });
     viewer.start();
-    var documentId = 'urn:' + urn;
+    var documentId = 'urn:'+urn;
     Autodesk.Viewing.Document.load(documentId, onDocumentLoadSuccess, onDocumentLoadFailure);
   });
 }
@@ -18,6 +18,18 @@ function onDocumentLoadSuccess(doc) {
   var viewables = doc.getRoot().getDefaultGeometry();
   viewer.loadDocumentNode(doc, viewables).then(i => {
     // documented loaded, any action?
+  });
+
+  viewer.addEventListener(Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT, function () {
+    const tree = viewer.model.getInstanceTree();
+    const rootId = tree.getRootId();
+    tree.enumNodeChildren(
+      rootId,
+      function (dbId) {
+        console.log(dbId, tree.getNodeName(dbId));
+      },
+      true
+    )
   });
 }
 
